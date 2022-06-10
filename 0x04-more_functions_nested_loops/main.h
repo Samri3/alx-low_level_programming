5900274945d8b680101daf136e2491e7193c449a
samrawite <samri21girma@gmail.com>
2022-06-10 12:49:29 -0700

    rG

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,11 @@
-#ifndef INV_MAIN_H
-#define INV_MAIN_H
+#ifndef INV_main_H
+#define INV_main_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
 
 #endif
