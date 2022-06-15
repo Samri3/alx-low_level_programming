1953f28057310cc7228035ef20d2f4c8863f5474
samrawite <samri21girma@gmail.com>
2022-06-15 12:47:39 -0700

    FDTsy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,15 @@
-#ifndef HOLBERTON_FUNTIONS
-#define HOLBERTON_FUNTIONS
+#ifndef MAIN_FUNTIONS
+#define MAIN_FUNTIONS
 
 int _putchar(char c);
 void reset_to_98(int *n);
-
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *
-		#endif
+char *cap_string(char *);
+
+#endif
